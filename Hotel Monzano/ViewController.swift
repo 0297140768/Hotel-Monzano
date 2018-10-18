83c42c7aac1f7ad9bbe5d1b6f012efe2b7b63cfc
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-18 20:16:59 +0300

    Initial Commit

---
+++ Hotel Monzano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotel Monzano
+//
+//  Created by Татьяна on 18.10.2018.
+//  Copyright © 2018 Татьяна. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
